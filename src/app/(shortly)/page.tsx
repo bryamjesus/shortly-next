d7c09abcf689b1f74d87ddb4eb065b130085cdb2
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-10 17:19:21 -0500

    Feat: Update route

--- src/app/(shortly)/page.tsx
+++ src/app/(shortly)/page.tsx
@@ -1,10 +1,3 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Dashboard',
-};
-
 export default function MainPage() {
   return <></>;
 }
