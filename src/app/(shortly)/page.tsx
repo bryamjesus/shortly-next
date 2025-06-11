ea0c7c0c69dff65e8809af6bedabc44d523690c1
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-11 16:48:46 -0500

    Feat: add form

--- src/app/(shortly)/page.tsx
+++ src/app/(shortly)/page.tsx
@@ -1,7 +1,16 @@
+import { FormShortUrl } from '@/components/form/FormShortUrl';
+import { geistMono } from '@/config/fonts';
+
 export default function MainPage() {
   return (
     <>
-      <h1>Main</h1>
+      <div>
+        <h2
+          className={`${geistMono.className} font-black text-xl md:text-2xl lg:text-3xl`}>
+          Acortar link
+        </h2>
+        <FormShortUrl />
+      </div>
     </>
   );
 }
