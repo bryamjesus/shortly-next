5622943260f49838ae9cb2d6dd67ddd0cf48509a
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-13 23:49:11 -0500

    Feat: add providers

---
+++ src/store/url/urlSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UrlState {
+  url: string;
+  codeShortUrl: string;
+}
+
+const initialState: UrlState[] = [];
+
+export const urlSlice = createSlice({
+  name: 'url',
+  initialState,
+  reducers: {
+    addUrlShort: (state, action: PayloadAction<UrlState>) => {
+      state.push(action.payload);
+    },
+    resetUrls: () => initialState,
+  },
+});
+
+export const { addUrlShort, resetUrls } = urlSlice.actions;
+
+export default urlSlice.reducer;
