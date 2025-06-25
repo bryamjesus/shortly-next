69308ec1be006a5390fc7d656be9795e387f9b6c
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-25 13:54:33 -0500

    Feat: update color

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <div className="bg-[#e0e0e0] flex w-full justify-center text-xs py-3">
+    <div className="bg-(--primary) text-(--light) flex w-full justify-center text-xs py-3">
       <Link href="/">
         <span className={`antialiased font-bold`}>Shortly </span>
         <span>| Link </span>
