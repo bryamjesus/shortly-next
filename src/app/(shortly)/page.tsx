28653e6323ce2464fbedecef9599d1a562f82901
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-26 17:09:12 -0500

    Feat: update

--- src/app/(shortly)/page.tsx
+++ src/app/(shortly)/page.tsx
@@ -4,13 +4,17 @@ import { geistMono } from '@/config/fonts';
 export default function MainPage() {
   return (
     <>
-      <div>
+      <div className="py-3">
         <h2
-          className={`${geistMono.className} font-black text-xl md:text-2xl lg:text-3xl`}>
-          Acortar link
+          className={`${geistMono.className} font-medium text-4xl md:text-5xl lg:text-6xl`}>
+          Acorta tus enlaces en segundos
         </h2>
-        <FormShortUrl />
+        <p className="pt-1 text-gray-600 text-[1rem] font-medium dark:text-gray-400">
+          Transforma URLs largas en enlaces cortos y memorables. Totalmente
+          gratuito.
+        </p>
       </div>
+      <FormShortUrl />
     </>
   );
 }
