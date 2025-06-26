8815ee896329e016342a02d199f6c34b9f62c0f3
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-26 16:47:39 -0500

    Feat: update navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ import { LinkIcon } from '../icons/LinkIcon';
 export const NavBar = () => {
   return (
     <>
-      <nav className="bg-(--primary) text-(--light) w-[100%] h-15">
+      <nav className="bg-(--primary) text-(--light) w-[100%] h-14">
         <div className="container h-full flex items-center">
           <Link href="/" className="flex items-center gap-0.5">
             <LinkIcon className="text-white w-8 h-8" color="#f9fafb" />
