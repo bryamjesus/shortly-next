5ed3bc3ba69841dd834bad55080a1f72de64984e
Bryam Talledo <bryam.talledo@outlook.com>
2025-06-28 12:33:27 -0500

    Feat: Update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <header className={`${geistMono.className} border-b border-general`}>
         <div className="container py-20">
           <h1
-            className={` font-medium text-4xl sm:text-5xl lg:text-6xl xl:text-8xl`}>
+            className={`font-medium text-4xl sm:text-5xl lg:text-6xl xl:text-8xl`}>
             Acorta y comparte tus enlaces en segundos.
           </h1>
           <p className="max-w-(--breakpoint-sm) pt-6 text-gray-600 text-lg/7 font-medium dark:text-gray-400">
@@ -15,7 +15,7 @@ export const Header = () => {
             enlaces luzcan limpios y fáciles de recordar.
           </p>
           <Link
-            className="inline-block btn-primary my-10 max-w-[375px] "
+            className="inline-block btn-primary my-10 max-w-[375px] text-sm/6"
             href={'#formUrl'}>
             Acortar
           </Link>
