e1c090bdac9d406e074f6fd3116a541ca08bddea
Bryam Talledo <bryam.talledo@outlook.com>
2025-07-08 18:06:42 -0500

    Feat: update feature

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,9 +1,9 @@
 import dynamicSettings from '@/data/feature.json';
 import { Container } from '../ui/container/Container';
 import { SubTitle } from '../ui/title/SubTitle';
+import { FeatureGrid } from './FeatureGrid';
 
 export const Features = () => {
-  console.log(dynamicSettings);
   return (
     <section id="features" className="py-20 border-t border-general">
       <Container>
@@ -11,11 +11,9 @@ export const Features = () => {
         <p className=" text-gray-600 dark:text-gray-400">
           La solucion mas simple y efectiva para acortar tus enlaces
         </p>
-        <div className="mt-16">
-          {dynamicSettings.feature.map((setting) => (
-            <li key={setting.id}>
-              <strong>{setting.name}:</strong> {setting.description}
-            </li>
+        <div className="mt-16 flex flex-wrap gap-4 justify-center lg:justify-between">
+          {dynamicSettings.feature.map(({ id, name, description }) => (
+            <FeatureGrid key={id} title={name} text={description} />
           ))}
         </div>
       </Container>
